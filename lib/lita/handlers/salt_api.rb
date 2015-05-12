7d4f4c96a25a51cc7e75fd3ad4a936e6ce9fdde2
rick <rick@localhost.localdomain>
2015-05-12 11:53:10 -0400

    Pushing Demo file up

--- lib/lita/handlers/salt_api.rb
+++ lib/lita/handlers/salt_api.rb
@@ -5,19 +5,45 @@ module Lita
     config :user
     config :pass	
     
-    route /^s(?:alt)it ping$/i, :test_ping, command: true, help: {
-      'saltit ping on server' => 'lists alive minions'}
-    route /^s(?:alt)it restart$/i, :service_restart, command: true, help: {
-      'saltit restart on server' => 'restarts services'}
-
+    route(/^saltit ping\s+(.+)/, :test_ping, command: true, help: {
+      "saltit ping on server" => "lists alive minions"})
+    route(/^saltit restart\s+(.+)/, :service_restart, command: true, help: {
+      "saltit restart on server" => "restarts services"})
+    route(/^saltops stop\s+(.+)/, :service_stop, command: true, help: {
+      "saltops stop on server" => "stops a service on a server"})
+    route(/^saltops start\s+(.+)/, :service_start, command: true, help: {
+      "saltops start on server" => "starts service on a server"})
+    route(/^saltops highstate\s+(.+)/, :highstate, command: true, help: {
+      "saltops highstate on server" => "runs a state.highstate on server"})
     def test_ping(response)
     	server = response.matches[0][1]
-    	response.reply "Add Ping Test"
+    	response.reply "True"
     end
 
     def service_restart(response)
     	server = response.matches[0][1]
-    	response.reply "Add service.restart"
+    	response.reply "True"
+    end	
+    
+    def service_start(response)
+    	server = response.matches[0][1]
+    	response.reply "True"
+    end	
+
+    def service_stop(response)
+    	server = response.matches[0][1]
+    	response.reply "True"
+    end	
+
+    def highstate(response)
+    	server = response.matches[0][1]
+    	response.reply "den2iws01:
+        Summary
+        -----------
+        Succeeded: 0
+        Failed:   0
+        -----------
+        Total:    0"
     end	
 
 #final end block
@@ -26,3 +52,4 @@ module Lita
     Lita.register_handler(SaltApi)
   end
 end
+
